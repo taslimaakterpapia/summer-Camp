e57996e111a93d51c77be5a5a4496bd38fd32c0c
taslimaakterpapia <taslimaakterpapia495@gmail.comm>
2023-06-17 22:12:50 +0600

    done

--- src/pages/Course/Course.jsx
+++ src/pages/Course/Course.jsx
@@ -9,10 +9,7 @@ const Course = ({course}) => {
                 <figure><img src="" alt="Shoes" /></figure>
                 <div className="card-body">
                   <h2 className="card-title">name</h2>
-                  {/* <p>{cl.instructor}</p>
-                  <p>{cl.price}</p>
-                  <p>{cl.available}</p> */}
-                  <div className="card-actions justify-end">
+                   <div className="card-actions justify-end">
                     {/* <button className="btn btn-primary">Read More</button> */}
                     <Link to={`singleclass/${_id}`}>
                         <button className="btn btn-neutral">View Details</button>
